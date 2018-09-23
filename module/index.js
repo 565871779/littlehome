f5a6d5316df130884930a7fa84fbab6a6860c470
1819764958 <1819764958@qq.com>
2018-09-23 10:07:21 +0800

    用户中心，管理员删除合并

--- module/index.js
+++ module/index.js
@@ -6,14 +6,21 @@ router.get('/', function(req,res){
 	data.admin = req.session.admin;
 	let sql = "SELECT * FROM tie WHERE `status` = 1 ORDER BY lastreplaytime; ";
 	conn.query(sql,function(err,results){
-		data.clist = results;
+		data.clist = results;	
 		
 		
     	res.render('./home',data);});
 	
 	})
-	
-	
-
-
+router.get('/tuichu',function(req,res){
+		let data={};
+		req.session.uname=0;//当用户退出的时候，清空session
+//		console.log(req.session.uname);
+//清空session后展示首页，重新获取数据库里面的首页上面相关值
+		let sql = "SELECT * FROM tie WHERE `status` = 1 ORDER BY lastreplaytime; ";
+		conn.query(sql,function(err,results){
+		data.clist = results;
+		data.uname = req.session.uname;
+    	res.render('./home',data);});
+})
 module.exports = router;
--- module/userCenter.js
+++ module/userCenter.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-
-
-
+router.get('/', function(req,res){
+	let data = {};
+	data.uname = req.session.uname;
+	let sql = "SELECT * FROM tie WHERE `status` = 1 ORDER BY lastreplaytime; ";
+	conn.query(sql,function(err,results){
+		data.clist = results;	
+    	res.render('./userCenter',data);});
+})
 module.exports = router;
