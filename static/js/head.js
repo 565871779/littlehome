d6fbb4f61180f36800fedbe197957c23563d46e6
508154277 <508154277@qq.com>
2018-09-25 17:23:52 +0800

    9.25 17:23

--- static/js/head.js
+++ static/js/head.js
@@ -1,34 +1,34 @@
-$(function(){
-	
-layui.use('element', function() {
-	var element = layui.element;
-});
-layui.use('form', function() {
-	var form = layui.form;
-});
-window.onscroll = function() {
-	let stop = document.documentElement.scrollTop || document.body.scrollTop;
-	if(stop >= 100) {
-		$(".layui-nav").css("position", "fixed");
-		$(".layui-nav").css("width", "100%");
-		$(".layui-nav").css("top", "0");
-	} else {
-		$(".layui-nav").css("position", "relative");
+$(function() {
+
+
+	layui.use('element', function() {
+		var element = layui.element;
+	});
+	layui.use('form', function() {
+		var form = layui.form;
+	});
+	window.onscroll = function() {
+		let stop = document.documentElement.scrollTop || document.body.scrollTop;
+		if(stop >= 100) {
+			$(".layui-nav").css("position", "fixed");
+			$(".layui-nav").css("width", "100%");
+			$(".layui-nav").css("top", "0");
+		} else {
+			$(".layui-nav").css("position", "relative");
+		}
 	}
-}
 
-
-//首页的搜索功能
-$('.search-btn').click(function() {
-//$.ajax({
-//	url: '/search',
-//	type: 'get',
-//	dataType: 'JSON',
-//	data:{keyword:$("#layui-input").val()},
-//	success: function(result) {
-//		window.open('/search.html');
-//	}
-//	})
-		window.location.href ="http://localhost:8088/search?keyword="+$("#layui-input").val();
+	//首页的搜索功能
+	$('.search-btn').click(function() {
+		//$.ajax({
+		//	url: '/search',
+		//	type: 'get',
+		//	dataType: 'JSON',
+		//	data:{keyword:$("#layui-input").val()},
+		//	success: function(result) {
+		//		window.open('/search.html');
+		//	}
+		//	})
+		window.location.href = "http://localhost:8088/search?keyword=" + $("#layui-input").val();
 	})
 })
--- static/js/img.js
+++ static/js/img.js
@@ -1,41 +1,43 @@
-//img
-let imgf = document.querySelectorAll('.imgf');
-let befor = document.querySelector('.befor');
-let next = document.querySelector('.next');
-let num = document.querySelectorAll('.num')
-console.log(imgf)
+window.onload = () => {
+	
+	$(".kanba").parent().removeClass("layui-this");
+	$(".tupian").parent().addClass("layui-this");
+	
+	let imgf = document.querySelectorAll('.imgf');
+	let befor = document.querySelector('.befor');
+	let next = document.querySelector('.next');
+	let num = document.querySelectorAll('.num')
+	console.log(imgf)
 
-imgf[0].style.display='block';
-    var a = 0;
-next.onclick=function(){
-	 if(a<imgf.length-1){  
-	 	a+=1;
-        imgf[a].style.display='block';	
-        a=a;
-        } 
-        else{
-        	a=imgf.length-1
-        }
+	imgf[0].style.display = 'block';
+	var a = 0;
+	next.onclick = function() {
+		if(a < imgf.length - 1) {
+			a += 1;
+			imgf[a].style.display = 'block';
+			a = a;
+		} else {
+			a = imgf.length - 1
+		}
 	}
-befor.onclick=function(){
-	 if(a>0){  
-	 	a-=1;
-        imgf[a].style.display='block';	
-        imgf[a+1].style.display='none'
-        a=a;
-        } 
-        else{
-        	a=0
-        }
+	befor.onclick = function() {
+		if(a > 0) {
+			a -= 1;
+			imgf[a].style.display = 'block';
+			imgf[a + 1].style.display = 'none'
+			a = a;
+		} else {
+			a = 0
+		}
 	}
 
-
- for(let j=0;j<num.length;j++){	
-    num[j].onclick=function(){		
-        for(let i=0;i<num.length;i++){
-    	imgf[i].style.display='none';
-		}		
-	    imgf[j].style.display='block';
-	    a=j;
-    }
+	for(let j = 0; j < num.length; j++) {
+		num[j].onclick = function() {
+			for(let i = 0; i < num.length; i++) {
+				imgf[i].style.display = 'none';
+			}
+			imgf[j].style.display = 'block';
+			a = j;
+		}
+	}
 }
